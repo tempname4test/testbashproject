c48f25cab54bff650bc9341aa703fc7750be5ede
Sergey Zavadsky <sergey.z@uveye.com>
2019-04-03 11:38:41 +0300

    fix for role

--- scripts/testentrty.sh
+++ scripts/testentrty.sh
@@ -2,4 +2,4 @@
 date=$(date)
 container=$(hostname)
 echo container id is $container > /var/www/html/index.nginx-debian.html
-echo date when you start the $date >> /var/www/html/index.nginx-debian.html
+echo date when container is build is $date >> /var/www/html/index.nginx-debian.html
